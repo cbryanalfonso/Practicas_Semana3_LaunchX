f494fee7f8f9eb6dbc60824a4ce7d9b7e212a4d7
cbryanalfonso <cbryanalfonso@gmail.com>
2022-05-02 21:34:24 -0500

    Add practice 5 twitter

---
+++ twitter/app/views/UserView.js
@@ -0,0 +1,14 @@
+const UserService = require("../services/UserService")
+
+class UserView {
+    static createUser(payload) {
+        if (payload === null) {
+            return {error: '/payload no existe/'}
+        }else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number'){
+            return UserService.create(payload.id, payload.username, payload.name)
+        }else{
+            return {error: "Error, las propiedades del payload necesitan tener un valor válido"}
+        }
+    }
+}
+module.exports = UserView
